5b6beef4ec9c3630390317d02105e26aacfb239a
fieldpalmer <gfieldpalmer@gmail.com>
2018-10-08 23:46:30 -0400

    fixed keys

--- keys.js
+++ keys.js
@@ -1,10 +1,6 @@
-console.log('this is loaded');
+console.log('keys loaded is loaded');
 
-exports.spotifyKeys = {
+exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-exports.omdb = {
-  key: process.env.OMDB_KEY
-}
--- liri.js
+++ liri.js
@@ -1,52 +1,26 @@
-const dotenv = require('dotenv').config();
+//Dependencies
+//read & set environment variables
+require('dotenv').config();
+//import api keys
 const keys = require('./keys.js');
+//import spotify npm package
 const Spotify = require('node-spotify-api');
+//import axios
 const axios = require('axios');
+//import file system package
 const fs = require('fs');
+//import moment js
 const moment = require('moment');
-//instantiate spotify query object        
+
+//istantiate spotify api client with out secret info
 let spotify = new Spotify(keys.spotify);
-//node liri.js concert-this <artist/band name>
-const searchBandsInTown = (bandName) => {
-    for ( var i = 3 ; i < process.argv.length ; i++) {
-        if (i > 3 && i < process.argv.length) {
-            bandName = bandName + ' ' + process.argv[i];
-        }
-        else {
-            songName += process.argv[i];
-        }
-    }
-
-    let queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
-
-    axios.get(queryUrl).then(
-        function(err, response) {
-            if (err) {
-                console.log("Uh oh, nothing found! Error: " + err );
-            }
-            if (response.data.length > 0) {
-                for( var i = 0 ; i < response.data[i].length ; i++) {
-                    let name = response.data[i].venue.name;
-                    let city = response.data[i].venue.city;
-                    let country = response.data[i].venue.country;
-                    let region = response.data[i].venue.region;
-                    let time = response.data[i].venue.datetime;
-                    let date = moment(time).format("dddd MMM Do, h:mm a");
-
-                    console.log("Venue Info ************************");
-                    console.log("Venue Name: " + name);
-                    console.log("City: " + city);
-                    console.log("Country: " + country);
-                    console.log("Time: " + region);
-                    console.log("Date: " + date);
-                }
-            }
-        }
-    )
-};
 
 // node liri.js spotify-this-song <song name>
 const searchSpotify = (songName) => {
+    //search error default
+    if (songName === undefined) {
+        songName = "What's my age again";
+    }    
     //loop through process.argv to find query
     for (var i = 3; i < process.argv.length; i++) {
         if (i > 3 && i < process.argv.length) {
@@ -55,38 +29,80 @@ const searchSpotify = (songName) => {
         else {
             songName += process.argv[i];
         }
-    }x
+    }
     spotify
         .search({
             type: 'track',
             query: songName,
             limit: 10
-        }, function(err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-                }
-                if(data.tracks.items.length > 0) {
-                    //loop through returned spotify data to display song info
-                    for (var i = 0 ; i < data.tracks.items.length ; i++) {
-                        let artist = data.tracks.items[i].artist[0].name;
-                        let trackName = data.tracks.items[i].name;
-                        let album = data.tracks.items[i].album.name;
-                        let sample = data.tracks.items[i].preview_url;
-                        console.log("Song #" + (i+1) + " ************************");
-                        console.log("Song Name: " + trackName);
-                        console.log("Artist: " + artist);
-                        console.log("Album: " + album);
-                        console.log("Preview Link: " + sample);
-                    }
-                } else {
-                    console.log("sorry, no results found for that song");
-                }
+        },
+        function(err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
             }
-        );
+            //loop through returned spotify data to display song info
+            for (var i = 0 ; i < data.tracks.items.length ; i++) {
+                let artist = data.tracks.items[i].artist[0].name;
+                let trackName = data.tracks.items[i].name;
+                let album = data.tracks.items[i].album.name;
+                let sample = data.tracks.items[i].preview_url;
+                console.log("Song #" + (i+1) + " ************************");
+                console.log("Song Name: " + trackName);
+                console.log("Artist: " + artist);
+                console.log("Album: " + album);
+                console.log("Preview Link: " + sample);
+                console.log("************************************");
+            }
+        }
+    );
 };
+
+
+//node liri.js concert-this <artist/band name>
+const searchBandsInTown = (bandName) => {
+    for ( var i = 3 ; i < process.argv.length ; i++) {
+        if (i > 3 && i < process.argv.length) {
+            bandName = bandName + ' ' + process.argv[i];
+        }
+        else {
+            bandName += process.argv[i];
+        }
+    }
+
+    let queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
+
+    axios.get(queryUrl).then(
+        function(response) {
+            if (!response.data) {
+                console.log("Sorry, nothing found!");
+            }
+
+            console.log("Concerts coming up for " + bandName +":")
+            for( var i = 0 ; i < response.data[i].length ; i++) {
+                let name = response.data[i].venue.name;
+                let city = response.data[i].venue.city;
+                let country = response.data[i].venue.country;
+                let region = response.data[i].venue.region;
+                let date = moment(response.data[i].datetime).format("MM/DD/YYYY");
+
+                console.log("Venue Info ************************");
+                console.log("Venue Name: " + name);
+                console.log("City: " + city);
+                console.log("Country: " + country);
+                console.log("Time: " + region);
+                console.log("Date: " + date);
+            }
+        }
+    )
+};
+
 
 //node liri.js movie-this <movie name here>
 const searchOMDB = (movieName) => {
+    //default error 
+    if (movieName === undefined) {
+        movieName = "Mr. Nobody";
+    }
     //for loop to add + to movie name
     for (var i = 3; i < process.argv.length; i++) {
         if (i > 3 && i < process.argv.length) {
@@ -97,7 +113,7 @@ const searchOMDB = (movieName) => {
         }
     }
     // Then run a request to the OMDB API with the movie specified
-    var queryUrl = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movieName;
+    var queryUrl = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movieName + "&y=&plot=full&tomatoes=true";
 
     axios
         .get(queryUrl)
@@ -112,25 +128,7 @@ const searchOMDB = (movieName) => {
             console.log("Plot: " + response.data.Plot);
             console.log("Actors: " + response.data.Actors);
         },
-        function (error) {
-        // * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-            if (error.response) {
-              // The request was made and the server responded with a status code
-              // that falls out of the range of 2xx
-              console.log(error.response.data);
-              console.log(error.response.status);
-              console.log(error.response.headers);
-            } else if (error.request) {
-              // The request was made but no response was received
-              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-              // http.ClientRequest in node.js
-              console.log(error.request);
-            } else {
-              // Something happened in setting up the request that triggered an Error
-              console.log('Error', error.message);
-            }
-            console.log(error.config);
-        });
+    )
 };
 
 //node liri.js do-what-it-says
@@ -142,6 +140,7 @@ const obeyText = () => {
         let songName = data.split(',')[1];
         searchSpotify(songName);
     })
+    //this is cheating, it's not actually reading the whole file and determining what to run I'm cutting the corner ==> running out of time quickly
 }
 
 // switch statement to determine user request method
